70da8d736ab6a1581d170c93ecc4bea1a096b5ce
Pawel Korus <pawelkorus@ymail.com>
2021-10-02 08:53:41 +0200

    split chunks optimisation

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,5 +3,16 @@ const common = require('./webpack.common.js');
 const path = require('path');
 
 module.exports = merge(common, {
-    mode: 'production'
+    mode: 'production',
+    optimization: { 
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: "vendor",
+                    chunks: "initial",
+                },
+            }, 
+        },
+    },
 });
