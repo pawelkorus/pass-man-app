9ba3b0b99b8bcd30909c747275c60bbfe5bb63c2
Pawel Korus <pawelkorus@ymail.com>
2022-04-10 00:46:56 +0200

    use createRoot api from React 18

--- src/index-local.tsx
+++ src/index-local.tsx
@@ -1,17 +1,17 @@
 import './main.scss';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client"
 
 import App from './App';
 import { ConfigProvider } from './service'
 import { S3RealmsProvider, ClientCredentialsAuthProvider } from './aws'
 
-ReactDOM.render(
+const root = createRoot(document.querySelector("#root"))
+root.render(
 <ConfigProvider> 
     <ClientCredentialsAuthProvider>
         <S3RealmsProvider>
             <App/>
         </S3RealmsProvider> 
     </ClientCredentialsAuthProvider>
-</ConfigProvider>, 
-    document.querySelector('#root'));
+</ConfigProvider>)
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
 import './main.scss';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from "react-dom/client"
 
 import App from './App';
 import { ConfigProvider } from './service'
 import { S3RealmsProvider } from './aws'
 import { IdentityPoolAuthProvider } from './aws'
 
-ReactDOM.render(
+const root = createRoot(document.querySelector("#root"))
+root.render(
 <ConfigProvider> 
     <IdentityPoolAuthProvider>
         <S3RealmsProvider>
             <App/>
         </S3RealmsProvider> 
     </IdentityPoolAuthProvider>
-</ConfigProvider>, 
-    document.querySelector('#root'));
+</ConfigProvider>)
